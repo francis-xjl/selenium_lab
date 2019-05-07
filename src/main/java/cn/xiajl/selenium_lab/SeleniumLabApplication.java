3a7da9592ebd45afab4f35de76a7bb67c315ed4b
francis <francis.xjl@qq.com>
2019-05-07 20:40:33 +0800

    Initial commit

---
+++ src/main/java/cn/xiajl/selenium_lab/SeleniumLabApplication.java
@@ -0,0 +1,13 @@
+package cn.xiajl.selenium_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xiajl/selenium_lab/Selenium01Test.java
@@ -0,0 +1,41 @@
+package cn.xiajl.selenium_lab;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * @author francis.xjl@qq.com
+ * @date 2019-05-07 19:44
+ **/
+public class Selenium01Test {
+
+    // 下载chromedriver: https://npm.taobao.org/mirrors/chromedriver/
+    public static final String CHROME_DRIVER_PATH = "/Users/xiajinlong/IdeaProjects/selenium_lab/install/74.0.3729.6/chromedriver";
+
+    @Test
+    public void test() {
+        // 设置chromedriver路径
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.jia.com/shanghai/");
+
+        // 录入建筑面积：80
+        driver.findElement(By.cssSelector("[name=area]")).sendKeys("80");
+//        driver.findElement(By.className("Jia_area_input")).sendKeys("80");
+
+        // 输入手机号
+        driver.findElement(By.cssSelector("[name=mobile]")).sendKeys("138001380000");
+
+        // 判断成功提示没有弹出
+        assert !driver.findElement(By.className("Jia_online_quote")).isDisplayed();
+
+        // 点击“立即获取”
+        driver.findElement(By.className("Jia_obtain_btn")).click();
+
+        // 判断成功提示弹出
+        assert driver.findElement(By.className("Jia_online_quote")).isDisplayed();
+
+    }
+}
